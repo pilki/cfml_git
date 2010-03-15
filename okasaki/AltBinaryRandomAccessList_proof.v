ce1762246a11825aadcfb51c10b92f2c18650cb9
arthurc <arthurc@ba02c806-7d2b-4818-9a48-35b22e6bdc79>
2010-03-15 17:16:11 +0000

    cleanupalt
    git-svn-id: svn://scm.gforge.inria.fr/svn/cfml@56 ba02c806-7d2b-4818-9a48-35b22e6bdc79

--- okasaki/AltBinaryRandomAccessList_proof.v
+++ okasaki/AltBinaryRandomAccessList_proof.v
@@ -3,12 +3,78 @@ Require Import FuncTactics LibCore.
 Require Import RandomAccessListSig_ml RandomAccessListSig_proof.
 Require Import AltBinaryRandomAccessList_ml.
 
-(* todo share *)
+(* todo: move *)
+
+Lemma pair_rep : forall `{Rep a1 A1} `{Rep a2 A2} (x:a1) (y:a2) X Y,
+  rep x X -> rep y Y -> rep (x, y) (X,Y).
+Proof. auto. Qed.
+
+Hint Resolve @pair_rep.
+
+Lemma cons_not_nil : forall A (x:A) l, x::l <> nil.
+Proof. auto_false. Qed.
+
+Hint Resolve cons_not_nil.
+
+
+Module AltBinaryRandomAccessListSpec <: RandomAccessListSigSpec.
+
+(** instantiations *)
+
+Module Import C <: MLRandomAccessList := MLAltBinaryRandomAccessList.
+Import MLAltBinaryRandomAccessList.
+
+(** invariant *)
+
 Fixpoint splitin A (Q:list (A*A)) : list A :=
   match Q with
   | nil => nil
   | (x,y)::Q' => x::y::(splitin Q')
   end.
+
+Inductive inv : forall `{Rep a A}, rlist a -> list A -> Prop :=
+  | inv_null : forall `{Rep a A},
+      inv _ Null nil
+  | inv_zero : forall `{Rep a A} ls Ls L',
+      inv _ ls Ls ->
+      L' =' splitin Ls ->
+      L' <> nil ->
+      inv _ (Zero ls) L'
+  | inv_one : forall `{Rep a A} x ls X Ls L',
+      rep x X ->
+      inv _ ls Ls ->
+      L' =' X :: splitin Ls ->
+      inv _ (One x ls) L'.
+
+(** model *)
+
+Implicit Arguments inv [[a] [A] [H]].
+
+Global Instance rlist_rep `{Rep a A} : Rep (rlist a) (list A).
+Proof.
+  intros. apply (Build_Rep inv).
+  introv HX HY. gen Y. 
+  induction HX; introv HY; gen_eq Y':Y; 
+   inverts HY; intros; subst; prove_rep.
+Defined.
+
+(** automation  *)
+
+Hint Constructors inv.
+Ltac auto_tilde ::= eauto.
+Hint Extern 1 (@gt nat _ _ _) => simpl; math.
+
+(** useful facts *)
+
+Fixpoint size a (l:rlist a) : nat :=
+  match l with
+  | Null => 1%nat
+  | Zero ls => (2 * (size ls))%nat
+  | One x ls => (2 * (size ls) + 1)%nat
+  end.
+
+Lemma size_pos : forall a (l:rlist a), (size l:int) > 0.
+Proof. induction l; simpl; math. Qed.
 
 Lemma splitin_last : forall A Q (x y:A),
   splitin (Q & (x,y)) = splitin Q ++ x::y::nil.
@@ -17,97 +83,20 @@ Proof.
   rew_list. simpl. rew_list. fequals.
 Qed.
 
-
-Module AltBinaryRandomAccessListSpec <: RandomAccessListSigSpec.
-
-(** instantiations *)
-
-Module Import C <: MLRandomAccessList := MLAltBinaryRandomAccessList.
-Import MLAltBinaryRandomAccesList.
-
-(** invariant *)
-
-Inductive inv : bool -> forall `{Rep a A}, rlist a -> list A -> Prop :=
-  | inv_null : forall `{Rep a A},
-      inv true _ Null nil
-  | inv_zero : forall `{Rep a A} ls Ls L' canstop,
-      inv false _ ls Ls ->
-      L' = splitin Ls ->
-      inv canstop _ (Zero ls) L'
-  | inv_one : forall `{Rep a A} x ls X Ls L' canstop,
-      rep x X ->
-      inv true _ ls Ls ->
-      L' = X :: splitin Ls ->
-      inv canstop _ (One x ls) L'.
-
-(** model *)
-
-Implicit Arguments inv [[a] [A] [H]].
-
-Global Instance rlist_rep `{Rep a A} : Rep (rlist a) (list A).
-Proof.
-  intros. apply (Build_Rep (fun l L => exists b, inv b l L)).
-  introv [bx HX] [by HY]. gen by Y. 
-  induction HX; introv HY; gen_eq Y':Y; 
-   inverts HY; intros; subst; prove_rep.
-Defined.
-
-(** automation  *)
-
-Hint Extern 1 (@rep (rlist _) _ _ _ _) => simpl.
-Hint Constructors inv.
-
-Ltac auto_tilde ::= eauto.
-
-(** useful facts *)
-
-Fixpoint size a (l:rlist a) : nat :=
-  match l with
-  | Null => 0%nat
-  | Zero ls => (2 * (size ls))%nat
-  | One x ls => (2 * (size ls) + 1)%nat
-  end.
-
-Lemma to_empty : forall `{Rep a A} L b,
-  inv b Null L -> L = nil.
+Lemma to_empty : forall `{Rep a A} L,
+  inv Null L -> L = nil.
 Proof. introv RL. set_eq L':L. inverts~ RL. Qed.
 
-Lemma from_empty : forall `{Rep a A} l b,
-  inv b l nil -> l = Null.
-Proof.
-  introv.
-(* gen_eq L: (@nil A). induction RQ; intros; subst. TODO*)
-Admitted.
-
-Lemma inv_weaken : forall `{Rep a A} l L,
-  inv false l L -> inv true l L.
-Proof. introv M. inverts M; constructors~. Qed.
-
-Hint Resolve @inv_weaken.
-
-Lemma inv_strengthen : forall `{Rep a A} l X L b,
-  inv b l (X::L) -> inv false l (X::L).
-Proof. introv M. inverts~ M. Qed.
-
-Hint Resolve @inv_strengthen.
-
-Lemma inv_strengthen' : forall `{Rep a A} l L b,
-  inv b l L -> L <> nil -> inv false l L.
-Proof. introv M. inverts~ M. auto_false. Admitted. (*evars*)
-
-Hint Resolve @inv_strengthen.
-
-
-(* H int Resolve (@rep (rlist _) _ _ _) => exists true.*)
-
+Lemma from_empty : forall `{Rep a A} l,
+  inv l nil -> l = Null.
+Proof. introv RL. inverts RL; auto_false. Qed.
 
 (** verification *)
 
 Lemma empty_spec : forall `{Rep a A},
   rep (@empty a) (@nil A).
 Proof.
-  intros. gen A H. apply (empty_cf a). xgo.
-  intros. exists~ true.
+  intros. gen A H. apply (empty_cf a). xgo~.
 Qed.
 
 Hint Extern 1 (RegisterSpec empty) => Provide empty_spec.
@@ -115,73 +104,50 @@ Hint Extern 1 (RegisterSpec empty) => Provide empty_spec.
 Lemma is_empty_spec : forall `{Rep a A},
   RepTotal is_empty (L;rlist a) >> bool_of (L = nil).
 Proof.
-  xcf. introv [b RL]. xgo.
+  xcf. introv RL. xgo.
   apply~ to_empty.
   intro_subst_hyp. applys C. apply~ from_empty.
 Qed.
 
 Hint Extern 1 (RegisterSpec is_empty) => Provide is_empty_spec.
 
-Lemma pair_rep : forall `{Rep a1 A1} `{Rep a2 A2} (x:a1) (y:a2) X Y,
-  rep x X -> rep y Y -> rep (x, y) (X,Y).
-Proof. auto. Qed.
-
-Hint Resolve @pair_rep.
-
-Hint Extern 1 (@gt nat _ _ _) => simpl; math.
-
 Lemma cons_spec : forall `{Rep a A},
   RepTotal cons (X;a) (L;rlist a) >> (X::L) ; rlist a.
 Proof.
-  intros. xintros. (* todo: intros useless *)
+  intros. xintros.
   intros x l. intros. gen_eq n: (size l). gen a A H x l X L.
   apply~ eq_gt_induction; clears n.
-  introv IH RX [b RL] N. subst n. xcf_app. xmatch.
+  introv IH RX RL N. subst n. xcf_app. xmatch.
   xgo. rewrite~ (to_empty RL).
-  xgo. simpl. inverts~ RL.
-  inverts RL.
-   specializes IH ((a*a)%type) __ __. xlet. fapplys IH; auto~. (* todo *)
-   destruct P_x1 as (b'&K). lets: (inv_strengthen K). xgo~.
+  xgo. inverts RL. subst~.
+  inverts RL. 
+   specializes IH ((a*a)%type) __ __.   
+   xlet. fapplys IH; auto~. (* todo: cleanup *)
+   subst. simpls. xgo~.
 Qed.
 
 Hint Extern 1 (RegisterSpec cons) => Provide cons_spec.
-(*
-Lemma rep_null : forall `{Rep a A},
-  rep Null (@nil A).
-Proof. exists~ __. Qed.
-Hint Resolve @rep_null.
-*)
-
-
-Lemma inv_weaken' : forall `{Rep a A} b l L,
-  inv b l L -> inv true l L.
-Proof. introv M. inverts M; constructors~. Qed.
-
 
 Lemma uncons_spec : forall `{Rep a A},
   RepSpec uncons (L;rlist a) |R>>
-     L <> nil -> R ((fun P => let (X,T) := P : A*list A in L = X::T) ;; a * rlist a).
+    L <> nil ->
+    R ((fun P => let (X,T) := P : A*list A in L = X::T) ;; a * rlist a).
 Proof.
-  intros. xintros. instantiate (1 := (a * rlist a)%type).
-   instantiate (1 := rlist a). xcf; auto.
-  intros l. intros. gen_eq n: (size l). gen a A H l L.
+  intros. xintros. intros l. intros. gen_eq n: (size l). gen a A H l L.
   apply~ eq_gt_induction; clears n.
-  introv IH [b RL] NE N. subst n. xcf_app. xmatch.
+  introv IH RL NE N. subst n. xcf_app. xmatch.
   xgo. applys NE. apply~ to_empty.
-  xgo. inverts RL. inverts H10. exists~ (X,@nil A).
-  xgo. inverts RL. exists~ (X,splitin Ls). splits~.
-   eapply pair_rep. eauto. exists __. constructors.
-  eapply @inv_strengthen'. eauto. intro_subst_hyp.
-  applys C0. rewrite~ (from_empty H10). auto. (* todo cleanup *)
-
-  inverts RL. specializes IH (a*a)%type __ __. xlet.
-  fapplys IH; eauto. (* todo: copy *) skip.
-  simpl. (* size pos when non empty *) skip.
-  xgo. destruct P_x1 as (((X,Y),Ls')&((RX&RY)&[b' RLs'])&EQL').
-  subst Ls. exists __. split~. apply~ @pair_rep.
-  exists __. constructors~. eapply @inv_weaken'; eauto.
-  simpl. eauto. 
-Admitted.
+  xgo. inverts RL as RX RLs. inverts RLs. exists~ (X,@nil A).
+  xgo. inverts RL as RX RLs. 
+    asserts: (splitin Ls <> nil). destruct Ls.
+     false C0. rewrite~ (from_empty RLs). destruct p; simple~.
+    xrep~ (X,splitin Ls).
+  inverts RL as RX RLs. asserts: (Ls <> nil). destruct~ Ls.
+   specializes IH (a*a)%type __ __.
+   xlet. fapplys IH; eauto. forwards~: (size_pos ps).
+   xgo. destruct P_x1 as (((Y,Z),Ls')&((RY&RZ)&RLs')&EQL').
+   subst Ls. xrep~.
+Qed.
 
 Hint Extern 1 (RegisterSpec uncons) => Provide uncons_spec.
 
@@ -191,7 +157,7 @@ Lemma head_spec : forall `{Rep a A},
 Proof.
   xcf. introv RQ NE.
   xlet. lets M: (>>> (@uncons_spec) a __ __). xapp~. (* todo *)
-  destruct _x0 as [x t]. destruct P_x0 as ([X T]&[? ?]&?). xgo~.
+  destruct _x0 as [x t]. destruct P_x0 as ([? ?]&[? ?]&?). xgo~.
 Qed.
 
 Hint Extern 1 (RegisterSpec head) => Provide head_spec.
@@ -202,7 +168,7 @@ Lemma tail_spec : forall `{Rep a A},
 Proof.
   xcf. introv RQ NE.
   xlet. lets M: (>>> (@uncons_spec) a __ __). xapp~. (* todo *)
-  destruct _x0 as [x t]. destruct P_x0 as ([X T]&[? ?]&?). xgo~.
+  destruct _x0 as [x t]. destruct P_x0 as ([? ?]&[? ?]&?). xgo~.
 Qed.
 
 Hint Extern 1 (RegisterSpec tail) => Provide tail_spec.
