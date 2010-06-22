e1e040aff5807603febbefa68166dff94d6bc0e0
arthurc <arthurc@ba02c806-7d2b-4818-9a48-35b22e6bdc79>
2010-06-22 14:14:17 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svn/cfml@123 ba02c806-7d2b-4818-9a48-35b22e6bdc79

--- sref/sref.ml
+++ sref/sref.ml
@@ -1,38 +1,84 @@
 
-type sref = unit ref
-
 let magic = Obj.magic
 
-let sref (v : 'a)  : sref =
-   (magic (ref v))
 
-let sget (l : sref) : 'a =
-   (!) (magic l)
+(** Standard manipulation of references *)
 
-let sset (l : sref) (v : 'a) : unit =
-   (:=) (magic l) v
+let ref : 'a -> 'a ref =
+   Pervasives.ref
 
-let sref_of_ref (l : 'a ref) : sref =
-   (magic l)
+let get : 'a ref -> 'a =
+   Pervasives.(!)
 
-let ref_of_sref (l : sref) : 'a ref =
-   (magic l)
+let set : 'a ref -> 'a -> unit =
+   Pervasives.(:=)
 
+
+(** Strong manipulation of references *)
+
+let alloc () : 'a ref =
+   magic (ref ())
+ 
+let sget (p : 'a ref) : 'b =
+   get (magic p)
+
+let sset (p : 'a ref) (v : 'b) : unit = 
+   set (magic p) v 
+
+let cast (p : 'a ref) : 'b ref =
+   magic p
+
+let cmp (p1 : 'a ref) (p2 : 'b ref) : bool =
+   (magic p1) == p2
+
+
+(** Nullable references *)
+
+let null_impl = ()
+
+let null () : 'a ref = 
+   magic null_impl
+
+let is_null (p : 'a ref) : bool =
+   p == (magic null_impl)
+
+
+(** Untyped references *)
+
+type sref = unit ref
+
+let sref (v : 'a) : sref =
+   magic (ref v)
+
+let salloc : unit -> sref =
+   alloc
+
+let snull : sref =
+   magic null
+
+
+(** Test *)
 
 let show n =
    Printf.printf "%d\n" n
 
 let () =
 
-   let x = sref 3 in
+   let a = null() in
+   if is_null a then show 1;
+   let b = ref 2 in
+   if not (is_null b) then show 2;
+
+   let x = salloc () in
+   sset x 3;
    let n : int = sget x in
    show n;
 
-   let y : int ref = ref_of_sref x in
+   let y : int ref = cast x in
    show (!y);
    show (sget x : int);
 
-   let z = sref_of_ref (ref 8) in
+   let z : sref = cast (ref 8) in
    show (sget z : int);
 
    sset x true;
