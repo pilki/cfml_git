859caf4d6225921886373b63d74a26389cdb85cf
arthurc <arthurc@ba02c806-7d2b-4818-9a48-35b22e6bdc79>
2010-08-19 12:58:23 +0000

    inout
    git-svn-id: svn://scm.gforge.inria.fr/svn/cfml@186 ba02c806-7d2b-4818-9a48-35b22e6bdc79

--- imper/InOut_proof.v
+++ imper/InOut_proof.v
@@ -24,12 +24,60 @@ Tactic Notation "xfor_general_manual" constr(I) :=
   let H := fresh "Hfor" in xfor_general_manual I as H.
 
 
+
+Tactic Notation "hextract" "as" simple_intropattern(I1) simple_intropattern(I2)
+ simple_intropattern(I3) simple_intropattern(I4) simple_intropattern(I5) := 
+  hextract as; intros I1 I2 I3 I4 I5. 
+Tactic Notation "hextract" "as" simple_intropattern(I1) simple_intropattern(I2)
+ simple_intropattern(I3) simple_intropattern(I4) simple_intropattern(I5)
+ simple_intropattern(I6) := 
+  hextract as; intros I1 I2 I3 I4 I5 I6. 
+Tactic Notation "hextract" "as" simple_intropattern(I1) simple_intropattern(I2)
+ simple_intropattern(I3) simple_intropattern(I4) simple_intropattern(I5)
+ simple_intropattern(I6) simple_intropattern(I7) := 
+  hextract as; intros I1 I2 I3 I4 I5 I6 I7. 
+
+
+Opaque Ref.
+
 (********************************************************************)
 
 Implicit Arguments dyn [[dyn_type]].
 
 Definition list_dyn A (L:list A) :=
   LibList.map dyn L.
+
+Lemma list_dyn_nil : forall A,
+  list_dyn (@nil A) = nil.
+Proof. auto. Qed.
+
+Lemma list_dyn_cons : forall A X (L:list A),
+  list_dyn (X::L) = (dyn X)::(list_dyn L).
+Proof. auto. Qed.
+
+Lemma list_dyn_last : forall A X (L:list A),
+  list_dyn (L&X) = (list_dyn L)&(dyn X).
+Proof. intros. unfold list_dyn. rew_list~. Qed.
+
+Hint Rewrite list_dyn_nil : rew_app.
+Hint Rewrite list_dyn_nil : rew_map.
+Hint Rewrite list_dyn_nil : rew_list.
+Hint Rewrite list_dyn_cons : rew_app.
+Hint Rewrite list_dyn_cons : rew_map.
+Hint Rewrite list_dyn_cons : rew_list.
+Hint Rewrite list_dyn_last : rew_app.
+Hint Rewrite list_dyn_last : rew_map.
+Hint Rewrite list_dyn_last : rew_list.
+
+Lemma length_app_nil2_inv : forall A (l:list A) l1 l2,
+  l = l1 ++ l2 -> 
+  length l = length l1 ->
+  l2 = nil.
+Proof.
+  intros. destruct l2. auto. subst l. rew_length in H0. math.
+Qed.
+
+Opaque Zplus LibList.fold_left.
 
 Lemma std_dup_sum_spec :
   Spec std_dup_sum () |R>> forall (n:int) (L:list int) Li Lo,  
@@ -48,15 +96,27 @@ Proof.
     (* forwards N: (@length_zero_inv _ L). math. subst L. rew_length in E. subst n. *)
     (* loop start *)
     hsimpl (0%Z) (@nil int) L.
-      unfold list_dyn. rew_list~.
+      rew_list~.
       splits. rew_length. math. auto. auto.
     (* loop body *)
-    
+    xextract as S L1 L2 (En&EL&ES).
+    destruct L2 as [|X L2']. rew_app in EL. subst L1. math. rew_app.
+    xapp. intro_subst.
+    xapp.
+    xapp. intro_subst.
+    xapp. hsimpl (L1 & X). 
+      rew_list~.
+      splits.
+        rew_length. math.
+        rew_app~.
+        rew_list. math.
     (* loop end *)
-  
-
-
-  
-
+    hextract as S L1 L2 (En&EL&ES).
+    forwards: (>>> length_app_nil2_inv EL). math. 
+    subst L2. rew_list in EL. subst L1.
+    rew_map. subst S. hsimpl.
+  (* conclusion *)
+  xapp. intro_subst.
+  xapp. hsimpl. rew_list~.
 Qed.
 
