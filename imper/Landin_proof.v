dc7905df03d7461fdc5c856704d9784f505647c9
arthurc <arthurc@ba02c806-7d2b-4818-9a48-35b22e6bdc79>
2010-08-19 08:29:28 +0000

    landin2
    git-svn-id: svn://scm.gforge.inria.fr/svn/cfml@183 ba02c806-7d2b-4818-9a48-35b22e6bdc79

--- imper/Landin_proof.v
+++ imper/Landin_proof.v
@@ -13,6 +13,12 @@ Proof.
   intros H Q MR. applys~ WR.
 Qed.
 
+Ltac xisspec_core ::=
+  solve [ 
+    let K := fresh "TEMP" in
+    lets K: sframe_is_spec_1; unfold is_spec_1, is_spec_0 in *;
+    intros_all; unfolds rel_le, pred_le; auto; auto* ].
+
 
 
 (********************************************************************)
@@ -28,15 +34,11 @@ Lemma landin_spec : forall A B,
     Rmain [] (fun f => Hexists I, I \* 
             [Spec f x |R>> forall y, sframe I (L y) x R]).
 Proof.
-  xcf. introv W Is Hbigf.
-  xapp.
+  xcf. introv W Is Hbigf. xapp.
   (* verification of G *)
   xfun (fun g => Spec g x |R>> forall (K:A->~~B->Prop) f',
-     is_spec_1 K ->
-     let K' := sframe (r ~> Ref Id f') K in
-     spec_1 K' f' -> 
-     K' x R).
-     intros_all. applys~ sframe_is_spec_1.
+     is_spec_1 K -> let K' := sframe (r ~> Ref Id f') K in
+     spec_1 K' f' -> K' x R). 
      intros SK K' Sf'. unfold K', sframe.
      applys SK. apply (spec_elim_1 Sf' x).
      intros H Q Happ. xapp. intro_subst. 
@@ -46,26 +48,25 @@ Proof.
      let I := (r ~> Ref Id f') in
      (Spec f' x' |R'>> forall y', lt (y',x') (y,x) -> sframe I (L y') x' R') ->
      sframe I (L y) x R).
-     intros_all. applys~ sframe_is_spec_1.
-     intros I Sf'. applys Is.
-       apply (spec_elim_2 (Hbigf I) g x y). xweaken.
-         intros_all. applys~ sframe_is_spec_1.
-         simpl. intros x' R LR SK y' Lt.
-         unfold sframe in SK|-*. unfold I. gen y'.
-         apply SK; [ | apply Sf' ].
-         introv H. intros. applys Is. apply~ H. auto.       
-       intros H Q Happ. apply Happ.
+    intros I Sf'. applys Is.
+      apply (spec_elim_2 (Hbigf I) g x y). xweaken.
+       simpl. intros x' R LR SK. unfold sframe in SK|-*.
+       apply SK; [ xisspec | apply Sf' ].
+      intros H Q Happ. apply Happ.
   (* tight the knot *)
   xapp. xret. hsimpl (r ~> Ref Id f).
   (* prove the spec of the result by induction *)
-  xinduction_heap W.
-      intros_all. applys sframe_is_spec_1. auto. apply H. auto.
-    xweaken.
-      intros_all. applys sframe_is_spec_1. auto. apply H. auto. auto.
-    auto.
+  xinduction_heap W. xweaken~.
 Qed.
 
 
+
+
+
+(*bin*)
+ (* intros_all. applys~ sframe_is_spec_1.*)
+ (*ntros_all. applys sframe_is_spec_1. auto. apply H. auto.*)
+(*intros_all. applys sframe_is_spec_1. auto. apply H. auto. auto.*)
 
 
 
